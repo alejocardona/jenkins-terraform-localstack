655e76261d198f2eef16a5b95a7996983208e5c6
Alejandro Cardona Gaviria <51707232+alejocardona@users.noreply.github.com>
2024-10-17 11:36:23 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,18 @@
 provider "aws" {
-  region = "us-east-1"
+  region                      = "us-east-1"
   skip_credentials_validation = true
-  skip_requesting_account_id = true
-  skip_metadata_api_check = true
-  s3_force_path_style = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
   endpoints {
     s3 = "http://localhost:4566/"
+  }
+}
+
+provider "aws" {
+  alias  = "localstack"
+  region = "us-east-1"
+  s3 {
+    force_path_style = true
   }
 }
 
